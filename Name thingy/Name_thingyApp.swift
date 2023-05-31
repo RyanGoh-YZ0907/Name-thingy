9ee4409561eea21eb5747e6fb2aa47610a710390
Ryan Goh Yu Ze <Ryan5yz@outlook.com>
2023-05-31 13:39:27 +0800

    Initial Committyjnyfyfjf

---
+++ Name thingy/Name_thingyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Name_thingyApp.swift
+//  Name thingy
+//
+//  Created by Ryan Goh on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Name_thingyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
