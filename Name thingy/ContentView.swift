3002401f532ba35200052e8d3ed8437643c3400e
Ryan Goh Yu Ze <Ryan5yz@outlook.com>
2023-05-31 13:47:50 +0800

    ieywgrfwieyasguegdf

--- Name thingy/ContentView.swift
+++ Name thingy/ContentView.swift
@@ -9,18 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Bello, world!")
+        TabView {
+            // Person 1 View
+            VStack {
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .padding()
+                
+                Text("This is person 1! Replace me with a view.")
+                    .padding()
+            }
+            .tabItem {
+                Label("Person 1", systemImage: "person.circle.fill")
+            }
+            
+            // Person 2 View
+            VStack {
+                Image(systemName: "person")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .padding()
+                
+                Text("This is person 2! Replace me with another view.")
+                    .padding()
+            }
+            .tabItem {
+                Label("Person 2", systemImage: "person")
+            }
+            
+            // Person 3 View
+            VStack {
+                Image(systemName: "person.fill.turn.down")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .padding()
+                
+                Text("This is person 3! Replace me with yet another view.")
+                    .padding()
+            }
+            .tabItem {
+                Label("Person 3", systemImage: "person.fill.turn.down")
+            }
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+@main
+struct NameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
     }
 }
